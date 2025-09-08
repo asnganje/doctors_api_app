4b7a10b7eb6ab3232648500347283a89cd2600c5
asnganje <abd.nganje@gmail.com>
2025-09-08 20:12:06 +0300

    setup user authentication using devise

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    render json: { message: "Logged in successfully", user: resource}, status: :ok
+  end
+
+   def respond_to_on_destroy(resource, _opts = {})
+    render json: { message: "Logged out successfully", user: resource}, status: :ok
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
