00e5eef521d11f6720661d8b627d4f0928a7daad
asnganje <abd.nganje@gmail.com>
2025-09-08 11:50:35 +0300

    add seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Doctor.create!(
+  [
+    {
+      name: "Dr. Saumu Omar",
+      specialization: "Crdiologist",
+      biography: "Has over 15 years of experience in treating heart-related conditions...",
+      picture: "https://example.com/images/dr-alice.jpg"
+    },
+    {
+      name: "Dr. John Mwangi",
+      specialization: "Dermatologist",
+      biography: "Dr. John Mwangi specializes in skin care and dermatological surgery...",
+      picture: "https://example.com/images/dr-john.jpg"
+    },
+    {
+      name: "Dr. Jane Otieno",
+      specialization: "Neurologist",
+      biography: "Dr. Jane Otieno is an expert in treating brain and nervous system conditions.",
+      picture: "https://example.com/images/dr-jane.jpg"
+    }
+]
+)
