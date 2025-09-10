a230b0fad9284c4657daf4de423bbb1f7405dfe4
asnganje <abd.nganje@gmail.com>
2025-09-10 19:44:32 +0300

    setup cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,22 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "http://localhost:5173"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+  allow do
+    # 🔑 Allow your production frontend (example: Render/Netlify/Vercel)
+    origins "https://your-frontend-domain.com"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
