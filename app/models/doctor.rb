f9d77530d4fdcefeb6464328ef07998d22a4da88
asnganje <abd.nganje@gmail.com>
2025-09-14 16:57:08 +0300

    add picture handling method in model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,7 @@
 class Doctor < ApplicationRecord
+  has_one_attached :picture
+
+  def picture_url
+    Rails.application.routes.url_helpers.rails_blob_url(picture, only_path: true) if picture.attached?
+  end
 end
