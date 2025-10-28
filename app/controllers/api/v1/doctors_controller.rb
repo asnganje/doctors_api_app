6d6c56dc539dc612f3183bcf5c5d77a08c1dd9ab
asnganje <abd.nganje@gmail.com>
2025-10-28 07:37:36 +0300

    modify create function to handle file upload to supabase

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -15,11 +15,25 @@ class Api::V1::DoctorsController < ApplicationController
   end
 
   def create
-    @doctor = Doctor.new(doctor_params)
-    if @doctor.save
-      render json: @doctor, status: :created
-    else
-      render json: { errors: @doctor.errors.full_messages }, status: :unprocessable_entity
+    file = params[:image]
+    return render json: {error: "No image provided"}, status: :bad_request unless file
+
+    begin
+      image_url = SupabaseService.upload_file(ENV["SUPABASE_BUCKET"], file)
+      
+      @doctor = Doctor.new(
+        name: params[:name],
+        specialization: params[:specialization],
+        biography: params[:biography],
+        image_url: image_url
+      )
+      if @doctor.save
+        render json: @doctor, status: :created
+      else
+        render json: { errors: @doctor.errors.full_messages }, status: :unprocessable_entity
+      end
+    rescue => e
+      render json: {error: e.message}, status: :internal_server_error
     end
   end
 
