e845aec86d715b5379025101c5968c80bf824b85
asnganje <abd.nganje@gmail.com>
2025-11-01 12:22:22 +0300

    do loggers for openai client

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -2,4 +2,4 @@ require "openai"
 key = ENV["OPENAI_API_KEY"] ||Rails.application.credentials.openai&.[](:api_key)
 raise "❌ Missing OpenAI API key in credentials" if key.blank?
 
-OPENAI_CLIENT = OpenAI::Client.new(access_token: key)
+OPENAI_CLIENT = OpenAI::Client.new(access_token: key, log_errors: true)
