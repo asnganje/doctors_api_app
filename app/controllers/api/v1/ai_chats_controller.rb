1b17a77bbca75536b80594c698476ff268b0573d
asnganje <abd.nganje@gmail.com>
2025-11-01 12:29:00 +0300

    test

--- app/controllers/api/v1/ai_chats_controller.rb
+++ app/controllers/api/v1/ai_chats_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::AiChatsController < ApplicationController
   def create
     prompt = params[:message]
     response = OpenaiChatService.new(prompt).call
+    Rails.logger.info "NGANJE #{prompt} ---- #{response}"
     render json: {reply: response}
   end
 end
