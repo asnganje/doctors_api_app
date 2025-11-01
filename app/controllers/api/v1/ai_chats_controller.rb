d18a8e5b5a010d0eb64a8c7822f1b8933eaa1c08
asnganje <abd.nganje@gmail.com>
2025-11-01 12:37:58 +0300

    add logger errors

--- app/controllers/api/v1/ai_chats_controller.rb
+++ app/controllers/api/v1/ai_chats_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AiChatsController < ApplicationController
   def create
     prompt = params[:message]
     response = OpenaiChatService.new(prompt).call
-    Rails.logger.info "NGANJE #{prompt} ---- #{response}"
     render json: {reply: response}
   end
 end
