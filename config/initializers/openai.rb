44c434ffe22ab4b8fcf9451d596e68919bb6af47
asnganje <abd.nganje@gmail.com>
2025-11-01 11:18:22 +0300

    Add env variable

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 require "openai"
-key = Rails.application.credentials.openai&.[](:api_key)
+key = ENV["OPENAI_API_KEY"]||Rails.application.credentials.openai&.[](:api_key)
 raise "❌ Missing OpenAI API key in credentials" if key.blank?
 
 OPENAI_CLIENT = OpenAI::Client.new(access_token: key)
