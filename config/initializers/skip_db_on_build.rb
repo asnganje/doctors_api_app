b0e3315bd867bace67b07d71b211c5b27cab6325
asnganje <abd.nganje@gmail.com>
2026-01-07 23:09:39 +0300

    remove skip file

--- config/initializers/skip_db_on_build.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.production? && ENV['DATABASE_URL'].blank?
-  module ActiveRecord
-    class Railtie < Rails::Railtie
-      rake_tasks do
-        Rake::Task['db:prepare'].clear
-      end
-    end
-  end
-end
