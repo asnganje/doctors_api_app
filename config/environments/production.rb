0228e5490587903076075c41457ff25d43fd1c51
asnganje <abd.nganje@gmail.com>
2026-01-10 23:21:55 +0300

    remove polling update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.solid_queue.dispatcher_options = { polling_interval: 10.seconds }
-  config.solid_queue.worker_options = { polling_interval: 5.seconds }
 end
